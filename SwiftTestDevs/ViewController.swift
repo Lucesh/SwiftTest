6c2bfcc87be6e35c10c85bf2e2b108a07fdb1f8e
Lucas Peters <saftpressenbedienungsanleitung@gmail.comrails>
2016-09-07 03:35:20 +0200

    Initial Commit

---
+++ SwiftTestDevs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTestDevs
+//
+//  Created by Lucas Peters on 07.09.16.
+//  Copyright © 2016 Lucas Peters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
